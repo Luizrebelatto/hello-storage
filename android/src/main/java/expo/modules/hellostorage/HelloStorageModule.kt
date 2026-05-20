dcdc55f18c1c01bb0389a05bdfbf3e3d0475b94f
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2026-05-19 22:48:05 -0300

    Refactor HelloStorageModule to streamline shared preferences access and simplify module definition

--- android/src/main/java/expo/modules/hellostorage/HelloStorageModule.kt
+++ android/src/main/java/expo/modules/hellostorage/HelloStorageModule.kt
@@ -1,50 +1,26 @@
 package expo.modules.hellostorage
 
+import android.content.Context
+import android.content.SharedPreferences
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import java.net.URL
 
 class HelloStorageModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('HelloStorage')` in JavaScript.
+  private val context
+    get() = appContext.reactContext ?: throw Exception("React context unavailable")
+
+  private val sharedPreferences: SharedPreferences
+    get() = context.getSharedPreferences("HelloStoragePrefs", Context.MODE_PRIVATE)
+
+  override func definition() = ModuleDefinition {
     Name("HelloStorage")
 
-    // Defines constant property on the module.
-    Constant("PI") {
-      Math.PI
+    AsyncFunction("setItem") { key: String, value: String ->
+      sharedPreferences.edit().putString(key, value).apply()
     }
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(HelloStorageView::class) {
-      // Defines a setter for the `url` prop.
-      Prop("url") { view: HelloStorageView, url: URL ->
-        view.webView.loadUrl(url.toString())
-      }
-      // Defines an event that the view can send to JavaScript.
-      Events("onLoad")
+    AsyncFunction("getItem") { key: String ->
+      return@AsyncFunction sharedPreferences.getString(key, null)
     }
   }
 }
